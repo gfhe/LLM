639b42df846124efdfb5bff01333a633eaa6dcff
gfhe <heguangfu21@mails.ucas.ac.cn>
2023-05-22 03:02:58 +0000

    Update: jupyterlab password and config

--- HuggingFace/build.sh
+++ HuggingFace/build.sh
@@ -1,3 +1,5 @@
 docker build . -t hgfkeep/llama:hf_torch_chat
 docker push hgfkeep/llama:hf_torch_chat
 
+docker build . -f Dockerfile_dev -t hgfkeep/llama:hf_torch_chat_dev
+docker push hgfkeep/llama:hf_torch_chat_dev
