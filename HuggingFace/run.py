011d2026da8589fb30494bd122d40bd5cdb0d4cd
gfhe <heguangfu21@mails.ucas.ac.cn>
2023-05-22 04:01:58 +0000

    Update: run support gpu

--- HuggingFace/run.py
+++ HuggingFace/run.py
@@ -1,17 +1,22 @@
 # 加载模型和tokenizer
 from transformers import AutoTokenizer, LlamaForCausalLM
 import os 
+import torch
 
 MODEL = os.environ.get('MODEL_LLAMA', './model')
-print(f"try load model @{MODEL}")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"try load model @{MODEL}, device={device}")
 
 model = LlamaForCausalLM.from_pretrained(MODEL)
 tokenizer = AutoTokenizer.from_pretrained(MODEL)
+model.to(device)
+tokenizer.to(device)
 
 prompt = input("[You]: ")
 
 while (prompt != 'q'):
     inputs = tokenizer(prompt, return_tensors="pt")
+    inputs.to(device)
 
     # Generate
     generate_ids = model.generate(inputs.input_ids, max_length=30)
