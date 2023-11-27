4fd852e7a071681a946a001badbf8c844a8dad14
gfhe <heguangfu21@mails.ucas.ac.cn>
2023-11-27 03:44:01 +0000

    Update llm-factory with auth keys

--- inference/lmql/build.sh
+++ inference/lmql/build.sh
@@ -3,3 +3,6 @@ docker push hgfkeep/lmql:cuda11.8
 
 docker build --build-arg GPU_ENABLED=true -f Dockerfile.serve -t hgfkeep/lmql:cuda11.8-serve .
 docker push hgfkeep/lmql:cuda11.8-serve
+
+
+envd build --output type=image,name=docker.io/hgfkeep/lmql,push=true
--- llm-factory/build.sh
+++ llm-factory/build.sh
@@ -4,5 +4,8 @@ apt install -y python3-pip
 pip install --upgrade pip
 pip install envd
 
+cp ../known_keys ./
 envd build --output type=image,name=docker.io/hgfkeep/vllm-factory:0.2-dev,push=true
 envd build -f :serve --output type=image,name=docker.io/hgfkeep/vllm-factory:0.2-runtime,push=true
+
+rm -rf ./known_keys
