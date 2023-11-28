ec0f4cc204276c7164398274f96b99a5afc2ccb6
gfhe <heguangfu21@mails.ucas.ac.cn>
2023-11-28 11:13:33 +0000

    llm-factory: support xformers

--- inference/vllm/build.sh
+++ inference/vllm/build.sh
@@ -3,6 +3,6 @@
 
 # docker buildx build . -f Dockerfile --build-arg base=nvidia/cuda:12.2.2-cudnn8-runtime-ubuntu22.04 -t hgfkeep/vllm:cudnn8-torch2.0.1
 # docker push hgfkeep/vllm:cudnn8-torch2.0.1
-
-envd build --output type=image,name=docker.io/hgfkeep/vllm:cuda12.2.2-cudnn8-devel,push=true
-envd build -f :serve --output type=image,name=docker.io/hgfkeep/vllm:cuda12.2.2-cudnn8-runtime,push=true
+# --export-cache type=registry,ref=hgfkeep/vllm:cache --import-cache type=registry,ref=hgfkeep/vllm:cache
+envd build --output type=image,name=docker.io/hgfkeep/vllm:cuda12.2.2-cudnn8-devel,push=true --export-cache type=registry,ref=hgfkeep/vllm:cache --import-cache type=registry,ref=hgfkeep/vllm:cache
+envd build -f :serve --output type=image,name=docker.io/hgfkeep/vllm:cuda12.2.2-cudnn8-runtime,push=true --export-cache type=registry,ref=hgfkeep/vllm:cache-run --import-cache type=registry,ref=hgfkeep/vllm:cache-run
