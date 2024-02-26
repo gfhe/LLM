9e90468f1dedc9bd9ab88404d61827e2af25fe6e
gfhe <heguangfu21@mails.ucas.ac.cn>
2024-02-26 03:44:13 +0000

    Update version

--- llm-factory/build.sh
+++ llm-factory/build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+LLM_FACTORY_VERSION=0.5.2
+
 apt install -y python3-pip
 pip install --upgrade pip
 pip install envd
 
-envd build --output type=image,name=docker.io/hgfkeep/vllm-factory:0.2-dev,push=true --ec type=registry,ref=docker.io/hgfkeep/vllm-factory:cache-dev --ic type=registry,ref=docker.io/hgfkeep/vllm-factory:cache-dev
-envd build -f :serve --output type=image,name=docker.io/hgfkeep/vllm-factory:0.2-runtime,push=true --ec type=registry,ref=docker.io/hgfkeep/vllm-factory:cache-run --ic type=registry,ref=docker.io/hgfkeep/vllm-factory:cache-run
-
+envd build --output type=image,name=docker.io/hgfkeep/vllm-factory:${LLM_FACTORY_VERSION}-dev,push=true --ec type=registry,ref=docker.io/hgfkeep/vllm-factory:cache-dev --ic type=registry,ref=docker.io/hgfkeep/vllm-factory:cache-dev
+envd build -f :serve --output type=image,name=docker.io/hgfkeep/vllm-factory:${LLM_FACTORY_VERSION}-runtime,push=true --ec type=registry,ref=docker.io/hgfkeep/vllm-factory:cache-run --ic type=registry,ref=docker.io/hgfkeep/vllm-factory:cache-run
