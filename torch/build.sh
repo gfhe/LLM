8ff5ccba5da9c97d402e0684b544be32b67f6150
gfhe <heguangfu21@mails.ucas.ac.cn>
2024-02-28 10:41:27 +0000

    Update

--- torch/build.sh
+++ torch/build.sh
@@ -6,7 +6,7 @@ buildTorch() {
     local v=$1
     local d=$2
     envd_func="torch_${d}_${v}"
-    echo "build torch-$v:$d, use envd function: $envd_func"
+    echo "build torch:${v}_$d, use envd function: $envd_func"
     envd build -f :$envd_func --output type=image,name=docker.io/hgfkeep/torch:${v}_$d,push=true --ec type=registry,ref=docker.io/hgfkeep/torch:${v}_$d-cache --ic type=registry,ref=docker.io/hgfkeep/torch:${v}_$d-cache
 }
 
