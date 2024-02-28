cd4ea5760baf5c830b2f84bf8e6e2f94154a8310
gfhe <heguangfu21@mails.ucas.ac.cn>
2024-02-28 03:05:41 +0000

    ADD: torch

---
+++ torch/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+versions=(cu118 cu122)
+
+buildTorch() {
+    local v=$1
+    local d=$2
+    envd_func="torch_${d}_${v}"
+    echo "build torch-$v:$d, use envd function: $envd_func"
+    envd build -f :$envd_func --output type=image,name=docker.io/hgfkeep/torch:${v}_$d,push=true --ec type=registry,ref=docker.io/hgfkeep/torch:${v}_$d-cache --ic type=registry,ref=docker.io/hgfkeep/torch:${v}_$d-cache
+}
+
+for v in "${versions[@]}"
+do
+  buildTorch $v dev
+  buildTorch $v runtime
+done
