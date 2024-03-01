f42312e5567c20b46cfdc16de2645bf2c4ffdd68
gfhe <heguangfu21@mails.ucas.ac.cn>
2024-03-01 05:07:43 +0000

    Update vllm

--- inference/vllm/build.sh
+++ inference/vllm/build.sh
@@ -7,7 +7,12 @@
 # docker push hgfkeep/vllm:cudnn8-torch2.0.1
 # --export-cache type=registry,ref=hgfkeep/vllm:cache --import-cache type=registry,ref=hgfkeep/vllm:cache
 
+docker login
 
-VLLM_VERSION=0.3.2
-envd build --output type=image,name=docker.io/hgfkeep/vllm:${VLLM_VERSION}-dev,push=true --export-cache type=registry,ref=hgfkeep/vllm:cache --import-cache type=registry,ref=hgfkeep/vllm:cache
-envd build -f :serve --output type=image,name=docker.io/hgfkeep/vllm:${VLLM_VERSION}-runtime,push=true --export-cache type=registry,ref=hgfkeep/vllm:cache-run --import-cache type=registry,ref=hgfkeep/vllm:cache-run
+VLLM_VERSION="0.3.2"
+TORCH_VERSION="t2.1"
+CUDA_VERSION="cu121"
+PYTHRON_VERSION="py311"
+
+envd build --output type=image,name=docker.io/hgfkeep/vllm:${VLLM_VERSION}-${PYTHRON_VERSION}-${TORCH_VERSION}-${CUDA_VERSION}-dev,push=true
+envd build -f :serve --output type=image,name=docker.io/hgfkeep/vllm:${VLLM_VERSION}-${PYTHRON_VERSION}-${TORCH_VERSION}-${CUDA_VERSION}-runtime,push=true
