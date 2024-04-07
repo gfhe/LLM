e24cfb044214206aee3a157b70141fb3fc1575c7
gfhe <heguangfu21@mails.ucas.ac.cn>
2024-04-07 09:18:54 +0000

    Update push to docker.io

--- basic/build.sh
+++ basic/build.sh
@@ -3,6 +3,6 @@
 pip3 install envd
 docker login
 tag="py311-cu12"
-envd build -f :py --output type=image,name=docker.io/hgfkeep/py:${tag}
+envd build -f :py --output type=image,name=docker.io/hgfkeep/py:${tag},push=true
 
-envd build -f :hf --output type=image,name=docker.io/hgfkeep/hf:${tag}
+envd build -f :hf --output type=image,name=docker.io/hgfkeep/hf:${tag}-hf,push=true
