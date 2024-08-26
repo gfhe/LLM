d86267414487ee08735e9d019c2b7b4dde6d8525
gfhe <90042648+gfhe@users.noreply.github.com>
2024-08-26 07:46:58 +0000

    更新basic的构建

--- basic/build.sh
+++ basic/build.sh
@@ -2,13 +2,14 @@
 
 pip3 install envd
 docker login -u hgfkeep
+basic="py311"
 tag="py311-cu12"
 
 # py37
 envd build -f :py37 --output type=image,name=docker.io/hgfkeep/py:37,push=true
 
 #py311
-envd build -f :py --output type=image,name=docker.io/hgfkeep/py:${tag},push=true
+envd build -f :py --output type=image,name=docker.io/hgfkeep/py:${basic},push=true
 
 # hf
 envd build -f :hf --output type=image,name=docker.io/hgfkeep/hf:${tag}-hf,push=true
